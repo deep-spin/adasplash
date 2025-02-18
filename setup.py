864df39e11db0db718b3afc402f3999167b7c2b8
Marcos Treviso <marcosvtreviso@gmail.com>
2025-02-18 08:38:58 +0000

    Bump version

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name="adasplash",
-    version="0.1.0",
+    version="0.1.1",
     author="Nuno Gonçalves, Marcos Treviso",
     author_email="marcosvtreviso@gmail.com",
     description="AdaSplash: Efficient Adaptive Sparse Attention in Triton",
